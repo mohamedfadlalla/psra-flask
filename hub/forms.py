b2931349e66e3225f6cf457cb7144631db5da266
Mohamed Fadlalla <mohamedbadrelmaarif@gmail.com>
2026-03-08 04:18:59 +0200

    refactor: remove job forms

--- hub/forms.py
+++ hub/forms.py
@@ -6,18 +6,6 @@ class MentorshipRequestForm(FlaskForm):
     message = TextAreaField('Message to Mentor', validators=[DataRequired(), Length(max=1000)])
     submit = SubmitField('Send Request')
 
-class JobForm(FlaskForm):
-    title = StringField('Job Title', validators=[DataRequired(), Length(max=255)])
-    company = StringField('Company Name', validators=[DataRequired(), Length(max=200)])
-    job_type = SelectField('Job Type', choices=[
-        ('internship', 'Internship'),
-        ('full_time', 'Full Time'),
-        ('part_time', 'Part Time')
-    ], validators=[DataRequired()])
-    location = StringField('Location', validators=[DataRequired(), Length(max=200)])
-    description = TextAreaField('Job Description', validators=[DataRequired(), Length(max=5000)])
-    skills = StringField('Required Skills (comma-separated)', validators=[Length(max=500)])
-    submit = SubmitField('Post Job')
 
 class ResearchProjectForm(FlaskForm):
     title = StringField('Project Title', validators=[DataRequired(), Length(max=255)])
@@ -30,6 +18,3 @@ class ApplicationForm(FlaskForm):
     motivation_letter = TextAreaField('Motivation Letter', validators=[DataRequired(), Length(max=2000)])
     submit = SubmitField('Submit Application')
 
-class JobApplicationForm(FlaskForm):
-    cover_letter = TextAreaField('Cover Letter', validators=[DataRequired(), Length(max=2000)])
-    submit = SubmitField('Submit Application')
